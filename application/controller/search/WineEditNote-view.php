9b76afeee7041cd6cd0915eb767a9abdee5369e6
ALeBear <ant-1@pouch.name>
2013-03-26 20:54:47 -0400

    Fixed the refresh and redirect issue after note edition

--- application/controller/search/WineEditNote-view.php
+++ application/controller/search/WineEditNote-view.php
@@ -1,4 +1,4 @@
-<form method="post" action="?c=<?php echo $code; ?>">
+<form method="post" data-ajax="false">
     <label for="appreciation"><?php echo $this->_('appreciation'); ?></label>
     <input type="range" name="appreciation" id="appreciation" value="<?php echo $note->getAppreciation();?>" min="0" max="100"/>
     <label for="note"><?php echo $this->_('note'); ?></label>
