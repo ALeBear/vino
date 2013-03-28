ebf95259893e223cdef4e603273847d4003c2939
ALeBear <ant-1@pouch.name>
2013-03-27 23:39:02 -0400

    Added routingPrefix to main css

--- application/layout-main.php
+++ application/layout-main.php
@@ -5,7 +5,7 @@
     <title><?php echo $title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.css" />
-    <link rel="stylesheet" href="/css/main.css" />
+    <link rel="stylesheet" href="<?php echo $routingPrefix; ?>/css/main.css" />
     <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
     <script src="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.js"></script>
     <?php echo $css; ?>
