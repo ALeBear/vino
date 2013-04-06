72388de7cf3ebaf392c5096daaf7f249f2e8e824
ALeBear <ant-1@pouch.name>
2013-04-05 22:24:27 -0400

    Made so that in iOS screen app mode the session is not reset when switching apps

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,5 +1,8 @@
 <?php
 
+//Set this so in iOS screen app mode the session is not reset when switching apps
+session_set_cookie_params(365 * 24 * 60 * 60);
+
 require __DIR__ . '/../vendor/autoload.php';
 
 use horses\Kernel;
